8ca834f869fa142722b888ccc0962d4678eac09a
Tom Runyon <thomas.runyon@saic.com>
2018-01-16 21:09:55 -0500

    Issue #1 to allow querying only specific resource objects and/or names

--- pkg/discovery/resources.go
+++ pkg/discovery/resources.go
@@ -4,7 +4,28 @@ import (
 	"k8s.io/client-go/kubernetes"
 	//cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"fmt"
 )
+
+var ErrResourceNotFound = fmt.Errorf("resouce requested not found in server")
+
+func GetResource(clientset *kubernetes.Clientset, resourceName string) (v1.APIResource, error) {
+	resources, e := clientset.Discovery().ServerPreferredResources()
+	if e != nil{
+		return v1.APIResource{}, e
+	}
+
+	for _, list := range resources{
+		for _, r := range list.APIResources{
+			if r.Name == resourceName || contains(r.ShortNames, resourceName) || r.SingularName == resourceName{
+				r.Version = list.GroupVersion
+				return r, nil
+			}
+		}
+	}
+	return v1.APIResource{}, ErrResourceNotFound
+
+}
 
 func GetResourceTypes(clientset *kubernetes.Clientset, groupVersion string) (resources []v1.APIResource, err error){
 
